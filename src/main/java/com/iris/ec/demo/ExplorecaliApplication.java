0121dc4bd8f92aa4f3934c940d298fe5b7f814ae
Ramesh Pettikattil govindankutty <rameshpg@Rameshs-MacBook-Pro-2.local>
2022-09-10 17:22:07 -0700

    Initial commit

---
+++ src/main/java/com/iris/ec/demo/ExplorecaliApplication.java
@@ -0,0 +1,13 @@
+package com.iris.ec.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExplorecaliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExplorecaliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iris/ec/demo/ExplorecaliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iris.ec.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExplorecaliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
